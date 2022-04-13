726439a44612f3a81ecabf5734551f5a28f33b35
Fernando <fom78a@gmail.com>
2022-04-13 19:28:04 -0300

    implementacion admin seteo de configuraciones

--- src/core/Routes.js
+++ src/core/Routes.js
@@ -2,6 +2,7 @@ import Socio from "components/Socio";
 import { Route, Routes } from "react-router-dom";
 import { Faq, Home, ListSocios, NotFound } from 'pages';
 import { ProtectedRoute } from "components/ProtectedRoute";
+import { Admin } from "pages/Admin";
 
 function AllRoutes() {
   return (
@@ -11,7 +12,7 @@ function AllRoutes() {
       <Route path="/faq" element={<Faq />} />
       <Route path="/list" element={<ProtectedRoute rolConAcceso="user"><ListSocios /> </ProtectedRoute>} />
       <Route path="/socio/:id" element={<Socio />} />
-      <Route path="/admin" element={<ProtectedRoute rolConAcceso="admin"><ListSocios /></ProtectedRoute>} />
+      <Route path="/admin" element={<ProtectedRoute rolConAcceso="admin"><Admin /></ProtectedRoute>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
